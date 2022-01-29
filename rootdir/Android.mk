f4bd230c3179fc3bffc771f9b8458be9128ed438
francoism90 <francoism90@users.noreply.github.com>
2022-01-29 17:06:11 +0100

    Initial commit

---
+++ rootdir/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := fstab.samsungexynos9810
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/fstab.samsungexynos9810
+LOCAL_MODULE_PATH       := $(TARGET_OUT_PRODUCT)/vendor_overlay/29/etc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.samsung.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.samsung.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.usb_accessory.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.usb_accessory.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
