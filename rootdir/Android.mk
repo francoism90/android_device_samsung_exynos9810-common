02f64fd8d223e58b2606552a595562963bf39821
francoism90 <francoism90@users.noreply.github.com>
2022-01-30 11:34:42 +0100

    Copy init to vendor

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,11 +17,43 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.baseband.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.baseband.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.samsung.bsp.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.samsung.bsp.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.samsung.rc
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.samsung.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.samsungexynos9810.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.samsungexynos9810.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.samsungexynos9810.usb.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.samsungexynos9810.usb.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -29,5 +61,37 @@ LOCAL_MODULE            := init.usb_accessory.rc
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.usb_accessory.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.vendor.onebinary.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.vendor.onebinary.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.vendor.rilchip.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.vendor.rilchip.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.vendor.rilcommon.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.vendor.rilcommon.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := mobicore.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/mobicore.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
