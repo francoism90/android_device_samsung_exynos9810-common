091fa4c42088a29faf06e95326872b5b90dbee4d
francoism90 <francoism90@users.noreply.github.com>
2022-01-30 09:25:56 +0100

    Add ueventd.rc + fix fstab efc/CPEFS

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,6 +9,14 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := ueventd.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := ueventd.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.samsung.rc
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
