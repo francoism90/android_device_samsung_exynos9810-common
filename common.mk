103f3a7e6328295dc83713a3d3351edde968c7b1
francoism90 <francoism90@users.noreply.github.com>
2022-01-31 20:07:13 +0100

    Replace camera

--- common.mk
+++ common.mk
@@ -76,7 +76,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64.exynos9810 \
+    android.hardware.camera.provider@2.5-service_64 \
     libsensorndkbridge \
     Snap
 
